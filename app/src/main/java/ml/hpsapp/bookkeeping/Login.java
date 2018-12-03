759a547c98cada343b8598508123015c8821bb2e
Hiren Panchani <hirenpanchani@hotmail.com>
2018-12-03 08:22:09 +0530

    Initial commit

---
+++ app/src/main/java/ml/hpsapp/bookkeeping/Login.java
@@ -0,0 +1,13 @@
+package ml.hpsapp.bookkeeping;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
